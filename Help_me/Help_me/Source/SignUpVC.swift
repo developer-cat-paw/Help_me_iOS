bbecc9c422b93f699edb84f7ac36837eb06f2587
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-02-07 17:45:24 +0900

    ✨ :: 회원가입 끝!!!!

--- Help_me/Help_me/Source/SignUpVC.swift
+++ Help_me/Help_me/Source/SignUpVC.swift
@@ -15,34 +15,48 @@ class SignUpVC: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var pwTextField: UITextField!
     @IBOutlet weak var pwChackTextField: UITextField!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
+        
     }
+    
+    
     @IBAction func SignUp(_ sender: Any) {
-        if let email = emailTextField.text ,let password = pwTextField.text {  // 옵셔널 바인딩
-            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in // 회원가입을 위한 함수
-                if error != nil{
+        if pwTextField.text == pwChackTextField.text {
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: pwTextField.text!
+            ) { (user, error) in
+                if user !=  nil{
+                    self.dismiss(animated: true)
+                }
+                else{
                     let alert = UIAlertController(
-                        title: "🚫회원가인 실패🚫",
+                        title: "🚫회원가입 실패🚫",
                         message: error?.localizedDescription,
                         preferredStyle: .alert)
                     alert.addAction(UIAlertAction(title: "확인", style: .default))
                     self.present(alert, animated: true, completion: nil)
-                } else {
-                    self.dismiss(animated: true)
                 }
             }
         }
+        else {
+            let errorAlert = UIAlertController(
+                title: "🚫회원가입 실패🚫",
+                mesçsage: "비밀번호를 확인해주세요!",
+                preferredStyle: .alert)
+            errorAlert.addAction(UIAlertAction(title: "확인", style: .default))
+            self.present(errorAlert, animated: true, completion: nil)
+        }
     }
     
-    
+    func passwordSecurety() {
+        pwTextField.isSecureTextEntry = true
+        pwChackTextField.isSecureTextEntry = true
+    }
     private func setupNavigationBar() {
         let navigationBarAppearance = UINavigationBarAppearance()
         navigationBarAppearance.backgroundColor = UIColor(red: 255.0/255, green:202.0/255, blue:40.0/255, alpha: 1)
         navigationController?.navigationBar.standardAppearance = navigationBarAppearance
         navigationController?.navigationBar.scrollEdgeAppearance = navigationBarAppearance
     }
-    
 }
